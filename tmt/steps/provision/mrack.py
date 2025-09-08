c95c1912b3ed93794830d1a5e7f21609a71d6c63
Lili Nie <lnie@redhat.com>
2025-09-08 17:15:11 +0800

    squash: address comment

--- tmt/steps/provision/mrack.py
+++ tmt/steps/provision/mrack.py
@@ -1411,9 +1411,7 @@ class GuestBeaker(tmt.steps.provision.GuestSsh):
 
         if self.job_id and self.primary_address:
             return
-        """
-        Create beaker job xml request and submit it to Beaker hub
-        """
+
         tmt_name = self._tmt_name()
         data = CreateJobParameters(
             tmt_name=tmt_name,
